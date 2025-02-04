926c096096779382db4cf42a5306885c08a9105a
Ramya B L <67257046+ram-ctrl@users.noreply.github.com>
2025-02-04 12:59:17 +0530

    Update outputs.tf

--- Three_tier_applocation/modules/rds_module/outputs.tf
+++ Three_tier_applocation/modules/rds_module/outputs.tf
@@ -1,9 +1,21 @@
-output "db_instance_endpoint" {
-  description = "The endpoint of the RDS instance"
+# Database Outputs
+output "database_endpoint" {
+  description = "The endpoint of the RDS database"
   value       = aws_db_instance.db_instance.endpoint
 }
 
-output "db_instance_identifier" {
-  description = "The identifier of the RDS instance"
-  value       = aws_db_instance.db_instance.id
+output "database_username" {
+  description = "The master username for the database"
+  value       = aws_db_instance.db_instance.username
+  sensitive   = true
+}
+
+output "database_port" {
+  description = "The port on which the database is listening"
+  value       = aws_db_instance.db_instance.port
+}
+
+output "database_security_group_id" {
+  description = "The security group ID assigned to the database"
+  value       = aws_security_group.db_sg.id
 }
