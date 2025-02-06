83b7fad8ffef1eb39b358ff07b83c3b4f6ba5517
Ramya B L <67257046+ram-ctrl@users.noreply.github.com>
2025-02-06 16:26:38 +0530

    Update aws_codedeploy.tf

--- Three_tier_applocation/cicd_pipeline/aws_codedeploy.tf
+++ Three_tier_applocation/cicd_pipeline/aws_codedeploy.tf
@@ -1,17 +1,24 @@
+# codedeploy.tf or in your codedeploy module
+
+# CodeDeploy Application Resource
 resource "aws_codedeploy_app" "my_app" {
   name = "my-app"
+  compute_platform = "Server"  # EC2 instances as deployment target
 }
 
+# CodeDeploy Deployment Group Resource
 resource "aws_codedeploy_deployment_group" "my_app_deployment_group" {
   app_name              = aws_codedeploy_app.my_app.name
   deployment_group_name = "my-app-deployment-group"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
-
-  deployment_config_name = "CodeDeployDefault.OneAtATime"
-
-  ec2_tag_filter {
-    key   = "Name"
-    value = "MyAppInstance"
-    type  = "KEY_AND_VALUE"
+  deployment_config_name = "CodeDeployDefault.OneAtATime"  # Or another configuration
+  
+  ec2_tag_set {
+    ec2_tag_filter {
+      key    = "Name"
+      value  = "my-ec2-instance"
+      type   = "KEY_AND_VALUE"
+    }
   }
+
+  service_role_arn = aws_iam_role.codedeploy_role.arn
 }
